69a0d4f5a6ee85d346edb4e153ae51a8682215b8
NEXGEN-AI038 <mukherjeesourav3091@gmail.com>
2026-08-22 23:03:51 +0530

    Fix ambient background stacking

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${display.variable} ${body.variable} ${mono.variable}`}>
-      <body>
+      <body className="isolate">
         <AmbientAiBackground />
         {children}
       </body>
